7828dc0140d6c423a4fc0f216c6bcb3a9d953654
Harshan <harshankrishdev003@gmail.com>
2023-06-12 01:58:19 +0530

    size changes

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,7 +1,6 @@
 import { NavLink } from "react-router-dom";
 import data from "../Data";
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
 const Product = () => {
   const [filter, setFilter] = useState([]);
   const filterProduct = (cat) => {
